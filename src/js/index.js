8161de540c8be101c432151b26d9234483e2c0fb
mohamedramy <mohamed.ramy20052014@gmail.com>
2022-10-18 13:38:52 +0200

    js

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,26 @@ function scroll(){
 window.onscroll= function(){
     scroll();
 }
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+  var year = new Date().getFullYear();
+  document.getElementById("year").innerHTML = year;
